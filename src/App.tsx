fd8158b6a10ca1f5090a5970bbbc5b9f82179dc7
dcattaneo <dcattaneo@gmail.com>
2024-12-30 23:37:14 +0100

    building components done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import "./App.css";
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
   let items = ["Cippa", "Lippa", "Zippa", "Pippa"];
+
+  const [showAlert, setShowAlert] = useState(false);
+
   return (
     <>
+      {showAlert && (
+        <Alert onClose={() => setShowAlert(false)}>
+          Alert <span>Text</span>
+        </Alert>
+      )}
+      <Button
+        onClick={function (): void {
+          console.log("Button clicked");
+          setShowAlert(!showAlert);
+        }}
+        color="danger"
+      >
+        Text Button
+      </Button>
       <ListGroup
         items={items}
         heading={"List Group"}
